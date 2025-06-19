1b5578bf272214d9a5a3b5adf520a6aae62fdf59
Gabry1211 <murronegabriele04@gmail.com>
2025-06-19 18:27:55 +0200

    Aggiunto il driver jdbc per la connessione al database

--- ecommerce_pc/src/main/java/model/DBConnection.java
+++ ecommerce_pc/src/main/java/model/DBConnection.java
@@ -5,12 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	private static final String URL = "jdbc:mysql://localhost:3306/nome_tuo_db?useSSL=false";
+	private static final String URL = "jdbc:mysql://localhost:3306/sito?useSSL=false";
     private static final String USER = "root";
     private static final String PASSWORD = "mysqlmerda";
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver JDBC non trovato", e);
+        }
+
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 
     }
-}
