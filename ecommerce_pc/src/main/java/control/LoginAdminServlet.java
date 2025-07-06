ba8ec0175549dff9c6faf39b08d178eed313c88a
Gabry1211 <murronegabriele04@gmail.com>
2025-07-06 17:26:00 +0200

    Aggiunta del file LoginAdminServlet.java

---
+++ ecommerce_pc/src/main/java/control/LoginAdminServlet.java
@@ -0,0 +1,50 @@
+package control;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.sql.*;
+import javax.naming.*;
+import javax.sql.DataSource;
+
+@WebServlet("/LoginAdminServlet")
+public class LoginAdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 String codiceFiscale = request.getParameter("codiceFiscale");
+	        String password = request.getParameter("password");
+
+	        try {
+	            Context initContext = new InitialContext();
+	            DataSource ds = (DataSource) initContext.lookup("java:comp/env/jdbc/ecommerceDB");
+	            Connection
+	            conn = ds.getConnection();
+
+	            String query = "SELECT * FROM Login WHERE Codice_Fiscale_Amministratore = ? AND Password = ?";
+	            PreparedStatement stmt = conn.prepareStatement(query);
+	            stmt.setString(1, codiceFiscale);
+	            stmt.setString(2, password);
+	            ResultSet rs = stmt.executeQuery();
+
+	            if (rs.next()) {
+	                HttpSession session = request.getSession();
+	                session.setAttribute("admin", codiceFiscale);
+	                response.sendRedirect("adminHome.jsp");
+	            } else {
+	                request.setAttribute("erroreLogin", "Credenziali errate");
+	                request.getRequestDispatcher("loginAdmin.jsp").forward(request, response);
+	            }
+
+	            conn.close();
+	        } catch (Exception e) {
+	            throw new ServletException("Errore login admin", e);
+	        }
+	}
+
+}
