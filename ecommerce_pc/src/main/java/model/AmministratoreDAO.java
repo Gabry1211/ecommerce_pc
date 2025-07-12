8d1889527e45e045b79a104c8f514bde2981d3df
Gabry1211 <murronegabriele04@gmail.com>
2025-07-12 18:34:45 +0200

    Aggiunta del file AmministratoreDAO.java

---
+++ ecommerce_pc/src/main/java/model/AmministratoreDAO.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class AmministratoreDAO {
+	public Amministratore doLogin(String codiceFiscale, String email) throws Exception {
+        Amministratore a = null;
+        Connection con = DBConnection.getConnection();
+
+        String sql = "SELECT * FROM Amministratore WHERE Codice_Fiscale = ? AND Email = ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, codiceFiscale);
+        ps.setString(2, email);
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            a = new Amministratore();
+            a.setCodiceFiscale(rs.getString("Codice_Fiscale"));
+            a.setNome(rs.getString("Nome"));
+            a.setIndirizzo(rs.getString("Indirizzo"));
+            a.setEta(rs.getInt("Eta"));
+            a.setEmail(rs.getString("Email"));
+            a.setDataDiNascita(rs.getDate("Data_Di_Nascita"));
+        }
+
+        rs.close();
+        ps.close();
+        con.close();
+        return a;
+    }
+
+}
