e5bbd6c547322742f4552f96223c92f61634f1fe
Gabriele <murronegabriele04@gmail.com>
2026-05-26 14:59:01 +0200

    Aggiunto il file HomeControl.java

---
+++ TechZone/src/main/java/control/HomeControl.java
@@ -0,0 +1,23 @@
+package control;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/Home")
+public class HomeControl extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/ProductControl").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
